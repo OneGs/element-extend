cc4854240a4bd838ec34e23bd6f9ba8467a74952
sunzeyu <1549522434@qq.com>
2023-11-10 09:40:49 +0800

    pref：character format

--- packages/virtual-tree/rollup.config.js
+++ packages/virtual-tree/rollup.config.js
@@ -12,41 +12,41 @@ const commonJs = require('@rollup/plugin-commonjs');
  * Rollup Configuration, 建议使用 defineConfig 以支持TS识别
  */
 module.exports = defineConfig([
-  {
-    input: 'index.js',
-    output: [
-      {
-        // import 'xxx'
-        dir: 'dist',
-        format: 'es',
-        entryFileNames: () => `[name].mjs`
-      }
-    ],
-    plugins: [
-      replace({
-        'process.env.NODE_ENV': JSON.stringify('production')
-      }),
-      nodeResolve(),
-      alias({
-        entries: [
-          {
-            find: '@',
-            replacement: path.resolve(__dirname, './src')
-          }
-        ]
-      }),
-      vue(),
-      postcss(),
-      babel({
-        babelHelpers: 'bundled', // 如果 vue 存在 jsx 语法，
-        extensions: ['.js', '.vue'] // 则会从 babel.config.js, 调用 @vue/babel-preset-jsx 处理
-      }),
-      commonJs()
-    ],
+	{
+		input: 'index.js',
+		output: [
+			{
+				// import 'xxx'
+				dir: 'dist',
+				format: 'es',
+				entryFileNames: () => `[name].mjs`
+			}
+		],
+		plugins: [
+			replace({
+				'process.env.NODE_ENV': JSON.stringify('production')
+			}),
+			nodeResolve(),
+			alias({
+				entries: [
+					{
+						find: '@',
+						replacement: path.resolve(__dirname, './src')
+					}
+				]
+			}),
+			vue(),
+			postcss(),
+			babel({
+				babelHelpers: 'bundled', // 如果 vue 存在 jsx 语法，
+				extensions: ['.js', '.vue'] // 则会从 babel.config.js, 调用 @vue/babel-preset-jsx 处理
+			}),
+			commonJs()
+		],
 
-    // 排除不需要混入代码中的第三方依赖
-    external: [
-      /^vue(\/.+|$)/ // 也可以字符串 'vue'
-    ]
-  }
+		// 排除不需要混入代码中的第三方依赖
+		external: [
+			/^vue(\/.+|$)/ // 也可以字符串 'vue'
+		]
+	}
 ]);
